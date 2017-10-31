8841032def5eee93952b8b2d382c43366f728af8
laileon <leon@igola.com>
2017-10-31 21:36:02 +0800

    fetch data from net

--- WeatherProject.js
+++ WeatherProject.js
@@ -5,28 +5,42 @@
  */
 
 import React, {Component} from 'react';
-import {
-    StyleSheet,
-    Text, TextInput,
-    View
-} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+
+import Forecast from "./Forecast"
+import OpenWeatherMap from "./open_weather_map"
 
 export default class WeatherProject extends Component<{}> {
     constructor(props) {
         super(props);
-        this.state = {zip: ""};
+        this.state = {zip: "", forecast: null};
     }
 
     _handleTextChange = event => {
-        this.setState({zip: event.nativeEvent.text});
+        let zip = event.nativeEvent.text;
+        OpenWeatherMap.fetchForecast(zip).then(forecast => {
+            console.log(forecast);
+            this.setState({forecast: forecast});
+        });
     };
 
     render() {
+        let content = null;
+        if (this.state.forecast !== null) {
+            content = (
+                <Forecast
+                    main={this.state.forecast.main}
+                    description={this.state.forecast.description}
+                    temp={this.state.forecast.temp}/>
+            );
+        }
+
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
                     You input {this.state.zip}.
                 </Text>
+                {content}
                 <TextInput
                     style={styles.input}
                     onSubmitEditing={this._handleTextChange}
@@ -41,7 +55,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#666666',
     },
     welcome: {
         fontSize: 20,
@@ -54,6 +68,6 @@ const styles = StyleSheet.create({
         padding: 2,
         height: 40,
         width: 100,
-        textAlign: 'center'
+        textAlign: 'center',
     },
 });
